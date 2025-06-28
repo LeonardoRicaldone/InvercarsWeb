0e4f11c47c4d8076241b728a193cad83ef656c53
Marvin Coto <20230644@ricaldone.edu.sv>
2025-06-28 13:47:41 -0600

    Functions for started and return conditions arrays has been added

--- backend/src/controllers/rentController.js
+++ backend/src/controllers/rentController.js
@@ -86,4 +86,69 @@ rentController.deleteRents = async(req, res) => {
     }
 }
 
+//Insert para agregar o reemplazar condiciones iniciales
+rentController.setStartedConditions = async (req, res) => {
+
+    //1- Pido los datos
+    const { rentId, fuelLevelStarted, mileageStarted } = req.body;
+
+    try {
+
+        const rent = await rentModel.findById(rentId);
+
+        if (!rent) {
+            return res.status(404).json({ message: 'Rent not found' });
+        }
+
+        const newCondition = { fuelLevelStarted, mileageStarted };
+
+        // Si ya hay una condición inicial, la reemplazamos
+        if (rent.startedConditions.length > 0) {
+            rent.startedConditions[0] = newCondition;
+        } else {
+            // Si no hay ninguna, la insertamos
+            rent.startedConditions.splice(0, 0, newCondition);
+        }
+
+        await rent.save();
+
+        res.status(200).json({ message: 'Started confidition added', rent });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', error });
+    }
+};
+
+// Insertar o reemplazar returnConditions
+rentController.setReturnConditions = async (req, res) => {
+
+    const { rentId, fuelLevelReturned, mileageReturned } = req.body;
+
+    try {
+
+        const rent = await rentModel.findById(rentId);
+
+        if (!rent) {
+            return res.status(404).json({ message: 'Renta no encontrada' });
+        }
+
+        const newCondition = { fuelLevelReturned, mileageReturned };
+
+        // Si ya hay una condición de retorno, la reemplazamos
+        if (rent.returnConditions.length > 0) {
+            rent.returnConditions[0] = newCondition;
+        } else {
+            // Si no hay ninguna, la insertamos
+            rent.returnConditions.splice(0, 0, newCondition);
+        }
+
+        await rent.save();
+        
+        res.status(200).json({ message: 'Condición de retorno registrada', rent });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error del servidor', error });
+    }
+};
+
 export default rentController;
