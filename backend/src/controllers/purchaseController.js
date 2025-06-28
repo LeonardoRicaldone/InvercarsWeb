937d5ce9a98ae766c3dd82ba8baa98bee73f709d
Marvin Coto <20230644@ricaldone.edu.sv>
2025-06-28 16:19:09 -0600

    Function and route for register direct purchase request has been added

--- backend/src/controllers/purchaseController.js
+++ backend/src/controllers/purchaseController.js
@@ -1,6 +1,7 @@
 import purchaseModel from "../models/Purchase.js"
 import purchaseRequestModel from "../models/PurchaseRequest.js"
 import financingModel from "../models/Financing.js"
+import rentController from "./rentController.js";
 
 //Creo un array de funciones vacías
 const purchaseController = {};
@@ -86,5 +87,36 @@ purchaseController.deletePurchases = async(req, res) => {
         return res.status(500).json({message: "Internal server error"})
     }
 }
+
+//paymentMethod transactionId paid paymentDate deliveryDate
+//Insert para agregar los detalles de ventas directas en el array
+purchaseController.addDirectPurchaseDetails = async(req, res) => {
+
+    //1- Pido los datos
+    const { purchaseId, paymentMethod, transactionId, paid, paymentDate, deliveryDate } = req.body;
+
+    try {
+        
+        const purchase = await purchaseModel.findById(purchaseId)
+
+        const newPurchaseDetails = { paymentMethod, transactionId, paid, paymentDate, deliveryDate };
+
+        //Si ya se había hecho un insert lo reemplazamos
+        if(purchase.directPurchase.length > 0) {
+            purchase.directPurchase[0] = newPurchaseDetails;
+        } else {
+            //Si no hay ninguna, lo inserto
+            purchase.directPurchase.splice(0, 0, newPurchaseDetails)
+        }
+
+        await purchase.save();
+
+        res.status(200).json({ message: 'Direct purchase details added', rent });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error', error });
+    }
+}
+
 
 export default purchaseController;
--- backend/src/routes/purchase.js
+++ backend/src/routes/purchase.js
@@ -13,4 +13,8 @@ router
   .put(purchaseController.updatePurchases)
   .delete(purchaseController.deletePurchases)
 
+
+  // Ruta para insertar o reemplazar los detalles de compras directas
+  router.post("/add-direct-purchase-details", rentController.setStartedConditions);
+  
 export default router;
